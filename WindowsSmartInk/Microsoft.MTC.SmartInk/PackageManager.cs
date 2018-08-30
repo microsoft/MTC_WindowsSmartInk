1790fb5487ba727ddf3a6ec4837a4d48fa27ae44
JeffBrand <jbrand@microsoft.com>
2018-08-30 16:17:32 -0500

    Package manager comments

--- WindowsSmartInk/Microsoft.MTC.SmartInk/PackageManager.cs
+++ WindowsSmartInk/Microsoft.MTC.SmartInk/PackageManager.cs
@@ -11,7 +11,9 @@ using Windows.Storage;
 
 namespace Micosoft.MTC.SmartInk.Package
 {
-
+    /// <summary>
+    /// Provides status of ONNX model download from Custom Vision service
+    /// </summary>
     public class DownloadProgressEventArgs : EventArgs
     {
         public double Percentage { get; set; }
@@ -19,28 +21,43 @@ namespace Micosoft.MTC.SmartInk.Package
         public double TotalBytes { get; set; }
     }
 
+    /// <summary>
+    /// PackageManager handles creation of Smart Ink Packages, package lookup and 
+    /// retrieval of ONNX models used by packages.  Also creates Nuget packages for 
+    /// distribution of Smart Ink Packages.
+    /// </summary>
     public class PackageManager
     {
         CancellationTokenSource _cts = new CancellationTokenSource();
         private IPackageManagerStorageProvider _provider;
-
- 
 
         public event EventHandler ModelDownloadStarted;
         public event EventHandler ModelDownloadCompleted;
         public event EventHandler ModelDownloadError;
         public event EventHandler<DownloadProgressEventArgs> ModelDownloadProgress;
 
+        /// <summary>
+        /// Create new instance of PackageManager using the LocalAppDataPackageManagerStorageProvider
+        /// </summary>
         public PackageManager()
         {
             _provider = new LocalAppDataPackageManagerStorageProvider();
         }
 
+        /// <summary>
+        /// Create a new instance of PackageManager using the provided instance of <c>IPackageManagerStorageProvider</c>
+        /// </summary>
+        /// <param name="provider">New instance of <see cref="IPackageManagerStorageProvider"/> of a storage provider</param>
         public PackageManager(IPackageManagerStorageProvider provider)
         {
             _provider = provider;
         }
 
+        /// <summary>
+        /// Opens existing package
+        /// </summary>
+        /// <param name="packageName">Name of package</param>
+        /// <returns>Exisiting smart ink package <see cref="SmartInkPackage"/>. Returns null if package does not exist.</returns>
         public Task<SmartInkPackage> OpenPackageAsync(string packageName)
         {
             if (string.IsNullOrWhiteSpace(packageName))
@@ -49,6 +66,12 @@ namespace Micosoft.MTC.SmartInk.Package
             return _provider.GetPackageAsync(packageName);
         }
 
+        /// <summary>
+        /// Creates a new Smart Ink Package
+        /// </summary>
+        /// <param name="packageName">Name of new package</param>
+        /// <param name="overwrite">Overwrites exisitng package.</param>
+        /// <returns>New smart ink package <see cref="SmartInkPackage"/>.</returns>
         public async Task<SmartInkPackage> CreatePackageAsync(string packageName, bool overwrite = false)
         {
             if (string.IsNullOrWhiteSpace(packageName))
@@ -61,6 +84,10 @@ namespace Micosoft.MTC.SmartInk.Package
             
         }
 
+        /// <summary>
+        /// Deletes existing package
+        /// </summary>
+        /// <param name="packageName">Name of package</param>
         public Task DeletePackageAsync(string packageName)
         {
             if (string.IsNullOrWhiteSpace(packageName))
@@ -69,6 +96,10 @@ namespace Micosoft.MTC.SmartInk.Package
             return _provider.DeletePackageAsync(packageName);
         }
 
+        /// <summary>
+        /// Gets a list of all installed packaged using the current <c>IPackageManagerStorageProvider</c>
+        /// </summary>
+        /// <returns><c>IList</c> of SmartInk Packages</returns>
         public async Task<IList<SmartInkPackage>> GetInstalledPackagesAsync()
         {
             var result = new List<SmartInkPackage>();
@@ -83,6 +114,11 @@ namespace Micosoft.MTC.SmartInk.Package
             return result;
         }
 
+        /// <summary>
+        /// Creates a Nuget package from a SmartInk Package
+        /// </summary>
+        /// <param name="package">Package to use for Nuget creation</param>
+        /// <param name="destination">File handle for saving the Nuget package</param>
         public async Task PublishPackageAsync(SmartInkPackage package, IStorageFile destination)
         {
             if (package == null)
@@ -113,16 +149,10 @@ namespace Micosoft.MTC.SmartInk.Package
          
             builder.Populate(metadata);
 
-            //var builder = new PackageBuilder();
-            //builder.Version = new NuGet.Versioning.NuGetVersion(new Version(package.Version));
             builder.ContentFiles.Add(new ManifestContentFiles() { Include = $"**/SmartInkPackages/*", CopyToOutput = "true", Flatten = "false", BuildAction="None" });
             builder.ContentFiles.Add(new ManifestContentFiles() { Include = $"**/SmartInkPackages/{package.Name}/*", CopyToOutput = "true", Flatten = "false", BuildAction = "None" });
             builder.ContentFiles.Add(new ManifestContentFiles() { Include = $"**/SmartInkPackages/{package.Name}/Model/*", CopyToOutput = "true", Flatten = "false" , BuildAction="None"});
             builder.ContentFiles.Add(new ManifestContentFiles() { Include = $"**/SmartInkPackages/{package.Name}/Icons/*", CopyToOutput = "true", Flatten = "false", BuildAction = "None" });
-            //builder.Title = $"SmartInk.{package.Name}";
-            //builder.Authors.Add("jab");
-            //builder.Owners.Add("jab");
-            //builder.Id = builder.Title;
 
             var root = $"{_provider.RootFolderPath}\\{package.Name}\\";
 
@@ -155,6 +185,12 @@ namespace Micosoft.MTC.SmartInk.Package
             
 
         }
+
+        /// <summary>
+        /// Download ONNX model from the uri provided
+        /// </summary>
+        /// <param name="modelUri">Uri to the ONNX model</param>
+        /// <returns><c>IStorageFile</c> of the downloaded ONNX file. <see cref="IStorageFile"/></returns>
         public async Task<IStorageFile> DownloadModelAsync(Uri modelUri)
         {
             if (modelUri == null)
@@ -173,6 +209,9 @@ namespace Micosoft.MTC.SmartInk.Package
 
         }
 
+        /// <summary>
+        /// Handle the Background download operation
+        /// </summary>
         private async Task HandleDownloadAsync(DownloadOperation download, bool start)
         {
 
@@ -201,7 +240,10 @@ namespace Micosoft.MTC.SmartInk.Package
             }
 
         }
-
+        /// <summary>
+        /// Progress of the background download
+        /// </summary>
+        /// <param name="operation"></param>
         private void DownloadProgress(DownloadOperation operation)
         {
             var currentProgress = operation.Progress;
