113b7294765cf3a11fae13e7ff02c191991e4350
JeffBrand <jbrand@microsoft.com>
2018-09-02 21:01:37 -0500

    smartinkmanifest property defaults updated

--- WindowsSmartInk/Microsoft.MTC.SmartInk/SmartInkManifest.cs
+++ WindowsSmartInk/Microsoft.MTC.SmartInk/SmartInkManifest.cs
@@ -6,9 +6,9 @@ namespace Micosoft.MTC.SmartInk.Package
     public class SmartInkManifest
     {
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; }= "Windows 10 SmartInk Package";
         public string Author { get; set; }
-        public string Version { get; set; }
+        public string Version { get; set; } = "1.0.0.0";
         public DateTimeOffset DatePublished{ get; set; }
         public Dictionary<Guid,string> IconMap { get; private set; } = new Dictionary<Guid, string>();
         public Dictionary<Guid, string> TagList { get; private set; } = new Dictionary<Guid, string>();
